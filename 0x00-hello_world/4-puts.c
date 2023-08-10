eaa7a19f53d8ee8f98748a5f1aad391d36673e07
Talabs8 <talabiodunn@gmail.com>
2023-08-10 11:50:46 -0700

    Second commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-int main ( ) 
+/**
+ * main - print a line of code using puts
+ * return: 0
+ */ 
+int main (void) 
 {
-
 	puts("\" Programming is like building a multilingual puzzle");
         return (0);
 }	
