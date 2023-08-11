2617b76bc251670036904bbf382ba96b61988a3b
Talabs8 <talabiodunn@gmail.com>
2023-08-11 10:14:44 -0700

    Final commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main - prints all possible different combinations of two digits
  * Return: ALways 0 (Success)
  */
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
@@ -7,21 +7,21 @@ int main(void)
 {
 int n, m, l;
 for (n = 48; n < 58; n++)
-{ 
+{
 for (m = 49; m < 58; m++)
-{ 
-for (l = 50; l < 58; l+)
+{
+for (l = 50; l < 58; l++)
 {
 if (l > m && m > n)
-{ 
+{
 putchar(n);
 putchar(m);
 putchar(l);
 if (n != 55 || m != 56)
-{ 
+{
 putchar(',');
 putchar(' ');
-} 
+}
 }
 }
 }
