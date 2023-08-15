4736090ea2c09b09f164726f1c330cdf30c18aaf
Talabs8 <talabiodunn@gmail.com>
2023-08-15 12:06:27 -0700

    A

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar("+");
+_putchar(char* "+");
 return (1);
 }
 else if (n == 0)
@@ -20,7 +20,7 @@ return (0);
 }
 else (n < 0)
 {
-_putchar("-");
+_putchar(char* "-");
 return (-1);
 }
 }
