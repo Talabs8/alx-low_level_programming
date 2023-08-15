da06ea8a837fe2c61f530534afe6cd3a499d9c24
Talabs8 <talabiodunn@gmail.com>
2023-08-15 07:32:09 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _putchar -  prints _putchar
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+void _putchar(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+}
