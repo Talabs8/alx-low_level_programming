aecf59b1503ac5d220d2e662045f94fbcf38380a
Talabs8 <talabiodunn@gmail.com>
2023-08-15 11:59:36 -0700

    P

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- *@n: character
+ *@n: character to be checked
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
@@ -18,7 +18,7 @@ else if (n == 0)
 _putchar("0");
 return (0);
 }
-else
+else (n < 0)
 {
 _putchar("-");
 return (-1);
