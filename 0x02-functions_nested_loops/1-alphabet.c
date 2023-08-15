44da19701e1478e7c249ecee63d87378123f0d66
Talabs8 <talabiodunn@gmail.com>
2023-08-15 08:41:52 -0700

    Alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
