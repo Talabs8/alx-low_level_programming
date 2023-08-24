f2168352695f4392de4171ebe337c7842cef5c63
Talabs8 <talabiodunn@gmail.com>
2023-08-24 00:40:10 -0700

    Corrections 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,15 @@
 #include "main.h"
 /**
- * _strcmp - function that compares two strings
+ * _strcmp - This function compares two strings
  * @s1: string 1 input to compare
  * @s2: string 2 input to be compared
- * return:int
+ * Return: 0 if s1 and s2 are equal
+ * negative integer if the stopping character
+ * in @s1 was less than the stopping
+ * character in @s2
+ * positive integer if the stopping character
+ * in @s1 was greater than the stopping
+ * character in @s2
  */
 int _strcmp(char *s1, char *s2)
 {
