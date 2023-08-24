9c6002fd348bdfebd896899a00ab1de96c99ec91
Talabs8 <talabiodunn@gmail.com>
2023-08-24 01:42:28 -0700

    Correction_3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,12 +4,7 @@
  * @s1: string 1 input to compare
  * @s2: string 2 input to be compared
  * Return: 0 if s1 and s2 are equal
- * negative integer if the stopping character
- * in @s1 was less than the stopping
- * character in @s2
- * positive integer if the stopping character
- * in @s1 was greater than the stopping
- * character in @s2
+ * else Return *s1 - *s2
  */
 int _strcmp(char *s1, char *s2)
 {
