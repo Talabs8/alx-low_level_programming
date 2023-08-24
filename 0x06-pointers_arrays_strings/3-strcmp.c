5644965560eec8e4670df6dcd7104508c32391b6
Talabs8 <talabiodunn@gmail.com>
2023-08-24 06:48:35 -0700

    Correction 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,19 @@
  * @s1: string 1 input to compare
  * @s2: string 2 input to be compared
  * Return: 0 if s1 and s2 are equal
- * else Return *s1 - *s2
+ * else Return *s1[i] - *s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
-while (((*s1 != '\0') && (*s2 != '\0'))  && (*s1 = *s2))
+int i;
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-s1++;
-s2++;
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
 }
-if (*s1 == *s2)
-{
+i++;
+}
 return (0);
-}
-else
-{
-return (*s1 - *s2);
-}
 }
