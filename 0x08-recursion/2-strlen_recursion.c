364a9fa25fa3502af91007a1d09c368a2c3a715b
Talabs8 <talabiodunn@gmail.com>
2023-08-30 07:26:09 -0700

    E

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,14 +2,14 @@
 /**
  * _strlen_recursion - length of string
  * @s:string
- * Return: int
+ * Return:int
  */
 int _strlen_recursion(char *s)
 {
 int i = 0;
 if (*s)
 {
-i++
+i++;
 i += _strlen_recursion(s + 1);
 }
 return (i);
