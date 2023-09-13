115c36a49bb3d59f0da1f53a2d86de9b68550e48
Talabs8 <talabiodunn@gmail.com>
2023-09-13 13:39:50 -0700

    :

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * get_op_func - get ops function pointer of type char array
+ * that accepts two inputs of int data type
+ * @s: a character pointer pointing to a symbol from the program argument
+ * Return: one of the operator functions to perform calculations
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{ "+", op_add },
+{ "-", op_sub },
+{ "*", op_mul },
+{ "/", op_div },
+{ "%", op_mod },
+{ NULL, NULL }
+};
+int i = 0;
+while (i < 5)
+{
+if (strcmp(s, ops[i].op) == 0)
+return (ops[i].f);
+i++;
+}
+return (0);
+}
